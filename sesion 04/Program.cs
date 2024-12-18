a8325d2cb83d34393766b7b405cd41e52155b8f1
moohammedddd <midorafat115566@gmail.com>
2024-12-18 05:26:16 +0200

    q 17

--- sesion 04/Program.cs
+++ sesion 04/Program.cs
@@ -57,6 +57,34 @@
 
             #endregion
 
+            #region 17 
+            // Console.WriteLine("Enter the coordinates of the first point (x1, y1): ");
+            // int x1 = int.Parse(Console.ReadLine());
+            // int y1 = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Enter the coordinates of the second point (x2, y2): ");
+            // int x2 = int.Parse(Console.ReadLine());
+            // int y2 = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Enter the coordinates of the third point (x3, y3): ");
+            // int x3 = int.Parse(Console.ReadLine());
+            // int y3 = int.Parse(Console.ReadLine());
+
+            // int area = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+            // if (area == 0)
+            // {
+            //     Console.WriteLine("The points lie on a single straight line.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The points do not lie on a single straight line.");
+            // }
+
+
+
+            #endregion
+
 
 
 

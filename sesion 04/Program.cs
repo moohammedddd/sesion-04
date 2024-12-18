31f420380df7bacfe93dfc4a70e16710ed58328d
moohammedddd <midorafat115566@gmail.com>
2024-12-18 05:24:02 +0200

    q 14

--- sesion 04/Program.cs
+++ sesion 04/Program.cs
@@ -15,6 +15,18 @@
             // System.Console.WriteLine(Reversed);
             #endregion
 
+            #region 14 - Write a program to allow the user to enter int and print the REVERSED of it.
+            //     System.Console.WriteLine("enter the string");
+            // string Input = Console.ReadLine();
+            // string Reversed  ="";
+            // for (int i = Input.Length -1; i >=0 ; i--)
+            // {
+            //     Reversed += Input[i]  ;
+            // }
+            // System.Console.WriteLine(Reversed);
+            #endregion
+
+
         }
     }
 }
